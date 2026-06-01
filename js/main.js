2d81f204a1a3d394fa743ab4e9a795b9a11c1b80
renz-dev <h1sokuuqqx@gmail.com>
2026-06-01 17:54:41 +0300

    dior start

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+  const swiperTop = new Swiper('.top__swiper', {
+  effect: 'fade',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+    const swiperAbout = new Swiper(".about__slider", {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      freeMode: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+
+
